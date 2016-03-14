f0ae9b059e89a4d7bb0abff57943db27a29355c6
Alex Lushpai <lushpai@gmail.com>
2016-03-14 20:58:37 +0300

    update API Documentation

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","RetailCrm\\ApiClient"],["c","RetailCrm\\Exception\\CurlException"],["c","RetailCrm\\Exception\\InvalidJsonException"],["c","RetailCrm\\Http\\Client"],["c","RetailCrm\\Response\\ApiResponse"]];
