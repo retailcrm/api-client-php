9ee1af85d5d47cb097a340e2d1c8235e1d358cc0
Sergey Chazov <oitv18@gmail.com>
2021-12-09 11:37:33 +0300

    add jms use

--- src/Model/Entity/Integration/Currency.php
+++ src/Model/Entity/Integration/Currency.php
@@ -1,7 +1,22 @@
 <?php
 
+/**
+ * PHP version 7.3
+ *
+ * @category Currency
+ * @package  RetailCrm\Api\Model\Entity\Integration
+ */
+
 namespace RetailCrm\Api\Model\Entity\Integration;
 
+use RetailCrm\Api\Component\Serializer\Annotation as JMS;
+
+/**
+ * Class Currency
+ *
+ * @category IntegrationModuleEditInfo
+ * @package  RetailCrm\Api\Model\Entity\Integration
+ */
 class Currency
 {
     /**
