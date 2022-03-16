d662ee2d9e1ee797cddf855c87db0f4dd9cd9128
Andrey Muriy <andrey.muriy@gmail.com>
2022-03-16 16:05:11 +0300

    Add update-scopes method to integration group

--- src/ResourceGroup/Integration.php
+++ src/ResourceGroup/Integration.php
@@ -11,8 +11,10 @@ namespace RetailCrm\Api\ResourceGroup;
 
 use RetailCrm\Api\Enum\RequestMethod;
 use RetailCrm\Api\Model\Request\Integration\IntegrationModulesEditRequest;
+use RetailCrm\Api\Model\Request\Integration\IntegrationModuleUpdateScopesRequest;
 use RetailCrm\Api\Model\Response\Integration\IntegrationModulesEditResponse;
 use RetailCrm\Api\Model\Response\Integration\IntegrationModulesGetResponse;
+use RetailCrm\Api\Model\Response\Integration\IntegrationModuleUpdateScopesResponse;
 
 /**
  * Class Integration
@@ -145,6 +147,73 @@ class Integration extends AbstractApiResourceGroup
             'integration-modules/' . $code . '/edit',
             $request,
             IntegrationModulesEditResponse::class
+        );
+        return $response;
+    }
+
+    /**
+     * Makes POST "/api/v5/integration-modules/{code}/update-scopes" request.
+     *
+     * Example:
+     * ```php
+     * use RetailCrm\Api\Factory\SimpleClientFactory;
+     * use RetailCrm\Api\Interfaces\ApiExceptionInterface;
+     * use RetailCrm\Api\Model\Entity\Integration\IntegrationModule;
+     * use RetailCrm\Api\Model\Entity\Integration\Integrations;
+     * use RetailCrm\Api\Model\Entity\Integration\Transport\TransportConfiguration;
+     * use RetailCrm\Api\Model\Request\Integration\IntegrationModulesEditRequest;
+     *
+     * $client = SimpleClientFactory::createClient('https://test.retailcrm.pro', 'apiKey');
+     *
+     * $requires = [
+     *     'integration_read',
+     *     'integration_write',
+     * ];
+     * $request = new IntegrationModuleUpdateScopesRequest($requires);
+     *
+     * try {
+     *     $response = $client->integration->edit('test-integration', $request);
+     * } catch (ApiExceptionInterface $exception) {
+     *     echo sprintf(
+     *         'Error from RetailCRM API (status code: %d): %s',
+     *         $exception->getStatusCode(),
+     *         $exception->getMessage()
+     *     );
+     *
+     *     if (count($exception->getErrorResponse()->errors) > 0) {
+     *         echo PHP_EOL . 'Errors: ' . implode(', ', $exception->getErrorResponse()->errors);
+     *     }
+     *
+     *     return;
+     * }
+     *
+     * echo 'Info: ' . print_r($response->info, true);
+     * ```
+     *
+     * @param string                                                                        $code
+     * @param \RetailCrm\Api\Model\Request\Integration\IntegrationModuleUpdateScopesRequest $request
+     *
+     * @return \RetailCrm\Api\Model\Response\Integration\IntegrationModuleUpdateScopesResponse
+     * @throws \RetailCrm\Api\Interfaces\ApiExceptionInterface
+     * @throws \RetailCrm\Api\Interfaces\ClientExceptionInterface
+     * @throws \RetailCrm\Api\Exception\Api\AccountDoesNotExistException
+     * @throws \RetailCrm\Api\Exception\Api\ApiErrorException
+     * @throws \RetailCrm\Api\Exception\Api\MissingCredentialsException
+     * @throws \RetailCrm\Api\Exception\Api\MissingParameterException
+     * @throws \RetailCrm\Api\Exception\Api\ValidationException
+     * @throws \RetailCrm\Api\Exception\Client\HandlerException
+     * @throws \RetailCrm\Api\Exception\Client\HttpClientException
+     */
+    public function updateScopes(
+        string $code,
+        IntegrationModuleUpdateScopesRequest $request
+    ): IntegrationModuleUpdateScopesResponse {
+        /** @var IntegrationModuleUpdateScopesResponse $response */
+        $response = $this->sendRequest(
+            RequestMethod::POST,
+            'integration-modules/' . $code . '/update-scopes',
+            $request,
+            IntegrationModuleUpdateScopesResponse::class
         );
         return $response;
     }
